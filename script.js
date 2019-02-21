520c52f8f39e41bbe3c36a631ed3ccb102ce9a48
theCoderSchool <tcsbcoach@gmail.com>
2019-02-21 15:58:44 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function accountCount(){
+  var num=document.getElementById("accountAmount");
+  num.innerHTML = 10000
+}
+function onlineCount(){
+  var num=document.getElementById("onlineAmount");
+  num.innerHTML =6
+}
